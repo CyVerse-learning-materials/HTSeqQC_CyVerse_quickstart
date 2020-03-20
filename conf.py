569439cc38fe73fbc053235090ba1051d0a9a94a
Renesh Bedre <rhbedre@gmail.com>
2020-03-19 19:17:01 -0500

    conf and substitution updated

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'Git for Mere Mortals as topic_tutorial'
-copyright = '2019, CyVerse'
-author = 'Meghan Balk', 'Ramona L. Walls'
+project = 'HTSeqQC'
+copyright = '2020, Renesh Bedre'
+author = 'Renesh Bedre', 'Niranjan Baisakh', 'Kranthi Mandadi'
 version = '1.0'
 release = '1.0'
 
